cafaba86d6148740f9bda6553d6a46e4bb4b1f1f
sushistack <kjm71712@naver.com>
2024-12-20 23:26:20 +0900

    modify crawl script

--- jobs/crawl.groovy
+++ jobs/crawl.groovy
@@ -10,6 +10,7 @@ job('crawl-job') {
             string('JASYPT_PASSWORD', 'jasypt-encryptor-password')
             string('DB_URL', 'datasource-url')
             string('DB_USERNAME', 'datasource-username')
+            string('DB_PASSWORD', 'datasource-password')
             string('DB_DRIVER_CLASS_NAME', 'datasource-driver-class-name')
         }
     }
@@ -24,6 +25,16 @@ job('crawl-job') {
             echo "\$DB_URL"
             echo "\$DB_USERNAME"
             echo "\$DB_DRIVER_CLASS_NAME"
+
+            JAVA_HOME=/Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home/bin/java \
+            -jar build/libs/*.jar \
+            --spring.batch.job.name=crawlJob \
+            --jasypt.encryptor.password=\$JASYPT_PASSWORD \
+            --spring.datasource.url=\$DB_URL \
+            --spring.datasource.username=\$DB_USERNAME \
+            --spring.datasource.password=\$DB_PASSWORD \
+            --spring.datasource.driver-class-name=\$DB_DRIVER_CLASS_NAME \
+            keywords=\$KEYWORDS_JSON
         """)
     }
 }
