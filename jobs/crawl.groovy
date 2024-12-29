afdf41945fdfabd42453e573e46ddddec6882273
shushistack <kjm71712@naver.com>
2024-12-29 17:37:03 +0900

    update crawl

--- jobs/crawl.groovy
+++ jobs/crawl.groovy
@@ -3,31 +3,11 @@ job('02-Crawl') {
     parameters {
         stringParam('KEYWORDS', '', 'crawled keywords')
     }
-    wrappers {
-        credentialsBinding {
-            string('JASYPT_PASSWORD', 'jasypt-encryptor-password')
-            string('DB_URL', 'datasource-url')
-            string('DB_USERNAME', 'datasource-username')
-            string('DB_PASSWORD', 'datasource-password')
-            string('DB_DRIVER_CLASS_NAME', 'datasource-driver-class-name')
-        }
-    }
     steps {
         shell("""
-            cd ${LINK_TREE_PRJ_DIR}
-            
             KEYWORDS_JSON=\$(echo "\$KEYWORDS" | jq -R 'split(",")' -c)
-            TIMESTAMP=\$(date +"%Y%m%d%H%M%S")
 
-            ${JAVA_HOME}/bin/java -jar build/libs/*.jar \
-            --spring.batch.job.name=crawlJob \
-            --jasypt.encryptor.password=\$JASYPT_PASSWORD \
-            --spring.datasource.url=\$DB_URL \
-            --spring.datasource.username=\$DB_USERNAME \
-            --spring.datasource.password=\$DB_PASSWORD \
-            --spring.datasource.driver-class-name=\$DB_DRIVER_CLASS_NAME \
-            keywords=\$KEYWORDS_JSON \
-            timestamp=\$TIMESTAMP
+            ${NODE_PATH}/bin/node ${LINK_TREE_CRAWLER_PRJ_DIR}/index.js --appHomeDir ${APP_HOME_DIR} --keywords \$KEYWORDS_JSON
         """)
     }
 }
