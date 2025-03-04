fcc570e9e39993703fb16dac88518703cd65f67b
shushistack <kjm71712@naver.com>
2025-03-04 23:38:21 +0900

    update crawl

--- jobs/crawl.groovy
+++ jobs/crawl.groovy
@@ -5,7 +5,7 @@ job('02-Crawl') {
     }
     steps {
         shell("""
-            KEYWORDS_JSON=\$(echo "\$KEYWORDS" | jq -R 'split(",")' -c)
+            KEYWORDS_JSON=\$(printf "%s" "\$KEYWORDS" | jq -R 'split(",")' -c)
 
             ${NODE_PATH}/bin/node ${LINK_TREE_CRAWLER_PRJ_DIR}/index.js --appHomeDir ${APP_HOME_DIR} --keywords \$KEYWORDS_JSON
         """)
